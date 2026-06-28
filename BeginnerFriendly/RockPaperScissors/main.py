d1553093a4d6ada1ecb30017e19ef80ceeb7b5d2
o7od <ozodtech7@gmail.com>
2026-06-28 00:30:43 -0400

    Python beginner projects

--- BeginnerFriendly/RockPaperScissors/main.py
+++ BeginnerFriendly/RockPaperScissors/main.py
@@ -37,31 +37,31 @@ scissors = '''
 ---.__(___)
 '''
 
-## This is to get the rock paper or scissors for the computer
-# list = [rock, paper, scissors]
+# This is to get the rock paper or scissors for the computer
+list = [rock, paper, scissors]
 
-# game_states = False
+game_states = False
 
-# while not game_states:
-#     ## this will generate a random index between 0 and the length of the list - 1
-#     random_int = random.randint(0, len(list) - 1)
+while not game_states:
+    ## this will generate a random index between 0 and the length of the list - 1
+    random_int = random.randint(0, len(list) - 1)
 
-#     userInput = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. \n"))
-#     print(list[userInput])
+    userInput = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. \n"))
+    print(list[userInput])
 
-#     print(f"Computer chose: \n{list[random_int]}")
+    print(f"Computer chose: \n{list[random_int]}")
 
-#     ## Logic of the game
-#     if userInput == random_int: 
-#         continue
-#     elif userInput - random_int == 1 or userInput - random_int == -2:
-#         print("You win")
-#         break
-#     elif random_int - userInput == 1 or random_int - userInput == -2:
-#         print("game over")
-#         break
-#     else: 
-#         continue
+    ## Logic of the game
+    if userInput == random_int: 
+        continue
+    elif userInput - random_int == 1 or userInput - random_int == -2:
+        print("You win")
+        break
+    elif random_int - userInput == 1 or random_int - userInput == -2:
+        print("game over")
+        break
+    else: 
+        continue
 
     
 
