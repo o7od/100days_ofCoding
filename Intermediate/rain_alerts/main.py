fa020684b0dd76b6a81d709acf9ada64be206716
o7od <ozodtech7@gmail.com>
2026-07-12 14:19:59 -0400

    fixed minor issue

--- Intermediate/rain_alerts/main.py
+++ Intermediate/rain_alerts/main.py
@@ -1,12 +1,10 @@
 import requests
-from datetime import datetime
 import os
 from twilio.rest import Client
 from dotenv import load_dotenv
 
+load_dotenv("twilio.env")
 
-
-api_id = "da0bd497992c76165a41bf148aa12e3f"
 URL = "https://api.openweathermap.org/data/2.5/forecast"
 LATITUDE = 40.610146
 LONGITUDE = -75.378791
@@ -14,7 +12,7 @@ LONGITUDE = -75.378791
 params = {
     "lat": LATITUDE,
     "lon": LONGITUDE,
-    "appid": api_id,
+    "appid": os.environ.get("API_ID"),
     "units": "metric",
     "cnt": 4
 }
@@ -28,21 +26,21 @@ weather_data = response.json()["list"]
 for each_day in weather_data:
     rain_or_not = int(each_day["weather"][0]["id"])
     if rain_or_not < 700:
+        print("True")
         rain_strength = each_day["weather"][0]["description"]
         time_rain = each_day["dt_txt"]
         # print(f"Do not forget to bring an umbrella")
 
-
         # Sending an sms notification to our phone 
-        load_dotenv("twilio.env")
         account_sid = os.environ.get("SID")
         auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
         client = Client(account_sid, auth_token)
 
         message = client.messages.create(
-            to="whatsapp: +998946124662",
-            from_="whatsapp: +17372583478",
-            body="It's going to rain tomorrow! Don't forget your umbrella ☔️",
+            to="whatsapp:+14848929589",
+            from_="whatsapp:+17372583478",
+            # message="It's going to rain tomorrow! Don't forget your umbrella ☔️",
+            content_sid="It's going to rain tomorrow! Don't forget your umbrella ☔️",
         )
 
 print(message.status)
